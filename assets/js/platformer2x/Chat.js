c06c91d3343d1fb0f28fef83fe81f047bdf2c463
AlexRubio1 <rubioalex688@gmail.com>
2024-03-20 14:07:04 -0700

    e

--- assets/js/platformer2x/Chat.js
+++ assets/js/platformer2x/Chat.js
@@ -13,7 +13,7 @@ class Chat {
         '911', 'die', 'luigi', 'peach', 'bowser', 'mario', 
         'mr.mortensen', 'mr. mortensen', 'mortensen', 'lopez', 
         'mr.lopez', 'mr. lopez','mister mortensen', 'mister lopez', 
-        'aws', 'amazonwebservices', 'amazon', 'amazonweb'];
+        'aws', 'amazonwebservices', 'amazon', 'amazonweb', 'immortal'];
 
         this.prohibitedWords.concat(wordsToAdd);
     }
